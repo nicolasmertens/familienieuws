ffa694453f11357638f6b95eaaec7076700ce892
Xavier Vermeiren <xavier@viloc.eu>
2013-12-01 17:19:25 +0100

    newspaper generation

--- application/module/Front/src/Front/Controller/FeedController.php
+++ application/module/Front/src/Front/Controller/FeedController.php
@@ -46,6 +46,32 @@ class FeedController extends AbstractActionController {
 
     public function pdfAction()
     {
+        $config    = $this->getServiceLocator()->get('config');
+        $config    = $config['connectors']['facebook'];
+
+        require_once './../application/lib/facebookwrapper.php';
+        require_once './../application/lib/fbphotofeed.php';
+
+        $facebookWrapper = new \FacebookWrapper(array(
+            'appId' => $config['app_id'],
+            'secret' => $config['app_secret'],
+            'cookie' => true,
+            'fileUpload' => false, // optional
+            'allowSignedRequest' => false // optional, but should be set to false for non-canvas apps
+        ));
+        $user = $facebookWrapper->getUserData();
+
+        if($user)
+        {
+            $fbPhotoFeed = new \FbPhotoFeed();
+            $fbPhotoFeed->addFeed($facebookWrapper->getUserUploadedPhotos(10));
+            $fbPhotoFeed->addFeed($facebookWrapper->getUserPhotos(10));
+        }
+        else
+        {
+            $fbPhotoFeed = null;
+        }
+        
         $view = new \Zend\View\Model\ViewModel(array(
             'config'      => $config,
             'fbPhotoFeed' => $fbPhotoFeed
@@ -55,11 +81,9 @@ class FeedController extends AbstractActionController {
         $viewRender = $this->getServiceLocator()->get('ViewRenderer');
         $html = $viewRender->render($view);
 
-        echo $html;die;
-
         $html2pdf = new \HTML2PDF('P', 'A4', 'nl');
         $html2pdf->pdf->SetDisplayMode('fullpage');
-        $html2pdf->writeHTML("<h1>test</h1>", false);
+        $html2pdf->writeHTML($html, false);
         $html2pdf->Output('test.pdf', false);
     }
 }
